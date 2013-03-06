c48e792bbeae17360fe7871ef3d63affcf6517b9
robash <robin@robash.me.uk>
2013-03-06 16:08:47 +0000

    Initial cleanup and commenting of configuration file

--- web/config.inc.php
+++ web/config.inc.php
@@ -1,286 +1,115 @@
 <?php
 
-// $Id: config.inc.php 2211 2011-12-24 09:27:00Z cimorrison $
-
 /**************************************************************************
- *   MRBS Configuration File
- *   Configure this file for your site.
- *   You shouldn't have to modify anything outside this file
- *   (except for the lang.* files, eg lang.en for English, if
- *   you want to change text strings such as "Meeting Room
- *   Booking System", "room" and "area").
+ * MRBS Configuration File
  **************************************************************************/
+
 
 /**********
  * Timezone
  **********/
- 
-// The timezone your meeting rooms run in. It is especially important
-// to set this if you're using PHP 5 on Linux. In this configuration
-// if you don't, meetings in a different DST than you are currently
-// in are offset by the DST offset incorrectly.
-//
-// Note that timezones can be set on a per-area basis, so strictly speaking this
-// setting should be in areadefaults.inc.php, but as it is so important to set
-// the right timezone it is included here.
-//
-// When upgrading an existing installation, this should be set to the
-// timezone the web server runs in.  See the INSTALL document for more information.
-//
-// A list of valid timezones can be found at http://php.net/manual/timezones.php
-// The following line must be uncommented by removing the '//' at the beginning
 $timezone = "Europe/London";
 
 
-/*******************
- * Database settings
- ******************/
-// Which database system: "pgsql"=PostgreSQL, "mysql"=MySQL,
-// "mysqli"=MySQL via the mysqli PHP extension
-// $dbsys = "mysql";
-// Hostname of database server. For pgsql, can use "" instead of localhost
-// to use Unix Domain Sockets instead of TCP/IP.
-// $db_host = "localhost";
-// Database name:
-// $db_database = "mrbs";
-// Database login user name:
-// $db_login = "mrbs";
-// Database login password:
-// $db_password = 'mrbs-password';
-// Prefix for table names.  This will allow multiple installations where only
-// one database is available
-// $db_tbl_prefix = "mrbs_";
-// Uncomment this to NOT use PHP persistent (pooled) database connections:
-// $db_nopersist = 1;
-require 'database_config.inc.php';
+/*****************
+ * Database config
+ *****************/
+require "database_config.inc.php";  // For security purposes our database details are stored in a separate file
 
 
-/* Add lines from systemdefaults.inc.php and areadefaults.inc.php below here
-   to change the default configuration. Do _NOT_ modify systemdefaults.inc.php
-   or areadefaults.inc.php.  */
+/*******
+ * Theme
+ *******/
+$theme = "theharbour";  // Our custom theme in the Theme folder
 
 
 /*********************************
  * Site identification information
  *********************************/
-$mrbs_admin = "The Harbour Administrator";
-$mrbs_admin_email = "The Harbour <harbour@robash.me.uk>";
-// NOTE:  there are more email addresses in $mail_settings below.    You can also give
-// email addresses in the format 'Full Name <address>', for example:
-// $mrbs_admin_email = 'Booking System <admin_email@your.org>';
-// if the name section has any "peculiar" characters in it, you will need
-// to put the name in double quotes, e.g.:
-// $mrbs_admin_email = '"Bloggs, Joe" <admin_email@your.org>';
-
-// The company name is mandatory.   It is used in the header and also for email notifications.
-// The company logo, additional information and URL are all optional.
-
-$mrbs_company = "The Harbour";   // This line must always be uncommented ($mrbs_company is used in various places)
-
-// Uncomment this next line to use a logo instead of text for your organisation in the header
-$mrbs_company_logo = "Themes/theharbour/harbour-logo.png";    // name of your logo file.   This example assumes it is in the MRBS directory
-
-// Uncomment this next line for supplementary information after your company name or logo
-//$mrbs_company_more_info = "You can put additional information here";  // e.g. "XYZ Department"
-
-// Uncomment this next line to have a link to your organisation in the header
-$mrbs_company_url = "http://the-harbour.co.uk.gridhosted.co.uk/mrbs-master/web";
-
-// This is to fix URL problems when using a proxy in the environment.
-// If links inside MRBS appear broken, then specify here the URL of
-// your MRBS root directory, as seen by the users. For example:
-// $url_base =  "http://webtools.uab.ericsson.se/oam";
-// It is also recommended that you set this if you intend to use email
-// notifications, to ensure that the correct URL is displayed in the
-// notification.
-$url_base = "http://the-harbour.co.uk.gridhosted.co.uk/mrbs-master/web";
+$mrbs_admin        = "The Harbour Administrator";
+$mrbs_admin_email  = "The Harbour <harbour@robash.me.uk>";
+$mrbs_company      = "The Harbour";                         // The name of our organisation
+$mrbs_company_logo = "Themes/$theme/logo.png";              // The logo displayed in the header
+$mrbs_company_url  = "http://www.the-harbour.co.uk/rooms";  // The URL used for the logo and home link
+$url_base          = "http://www.the-harbour.co.uk/rooms";
 
 
-/*******************
- * Themes
- *******************/
+/*************************
+ * Authentication settings
+ *************************/
 
-// Choose a theme for the MRBS.   The theme controls two aspects of the look and feel:
-//   (a) the styling:  the most commonly changed colours, dimensions and fonts have been 
-//       extracted from the main CSS file and put into the styling.inc file in the appropriate
-//       directory in the Themes directory.   If you want to change the colour scheme, you should
-//       be able to do it by changing the values in the theme file.    More advanced styling changes
-//       can be made by changing the rules in the CSS file.
-//   (b) the header:  the header.inc file which contains the function used for producing the header.
-//       This enables organisations to plug in their own header functions quite easily, in cases where
-//       the desired corporate look and feel cannot be changed using the CSS alone and the mark-up
-//       itself needs to be changed.
-//
-//  MRBS will look for the files "styling.inc" and "header.inc" in the directory Themes/$theme and
-//  if it can't find them will use the files in Themes/default.    A theme directory can contain
-//  a replacement styling.inc file or a replacement header.inc file or both.
-
-// Available options are:
-
-// "default"        Default MRBS theme
-// "classic126"     Same colour scheme as MRBS 1.2.6
-// "theharbour"     Custom theme created for The Harbour
-
-$theme = "theharbour";
-
-
-/***********************************************
- * Authentication settings - read AUTHENTICATION
- ***********************************************/
-
-$auth["type"] = "db"; // How to validate the user/password. One of "none"
-                          // "config" "db" "db_ext" "pop3" "imap" "ldap" "nis"
-                          // "nw" "ext".
-
-$auth['deny_public_access'] = TRUE; // Prevent the public (ie un-logged in users) from being able to view bookings
+$auth['deny_public_access'] = TRUE;  // Prevent the public, i.e. users who are not logged in, from being able to access the site and view bookings
+$auth["type"]               = "db";  // Store authentication information in the database
 
 
 /******************
  * Display settings
  ******************/
-
-// [These are all variables that control the appearance of pages and could in time
-//  become per-user settings]
-
-// Start of week: 0 for Sunday, 1 for Monday, etc.
-$weekstarts = 1;
-
-// Days of the week that should be hidden from display
-// 0 for Sunday, 1 for Monday, etc.
-// For example, if you want Saturdays and Sundays to be hidden set $hidden_days = array(0,6);
-//
-// By default the hidden days will be removed completely from the main table in the week and month
-// views.   You can alternatively arrange for them to be shown as narrow, greyed-out columns
-// by editing the CSS file.   Look for $column_hidden_width in mrbs.css.php.
-//
-// [Note that although they are hidden from display in the week and month views, they 
-// can still be booked from the edit_entry form and you can display the bookings by
-// jumping straight into the day view from the date selector.]
-$hidden_days = array(0,6);
-
-// Trailer date format: 0 to show dates as "Jul 10", 1 for "10 Jul"
-$dateformat = 1;
-
-// Define default starting view (month, week or day)
-// Default is day
-$default_view = "month";
-
-// Define clipping behaviour for the cells in the month view.                           
-// Set to TRUE if you want the cells in the month view to scroll if there are too
-// many bookings to display; set to FALSE if you want the table cell to expand to
-// accommodate the bookings.   (NOTE: (1) scrolling doesn't work in IE6 and so the table
-// cell will always expand in IE6.  (2) In IE8 Beta 2 scrolling doesn't work either and
-// the cell content is clipped when $month_cell_scrolling is set to TRUE.)
-$month_cell_scrolling = FALSE;
-
-// Max slots = 13 hours (8am to 9pm) + 1 hour (default booking) * 6 (10 minute slots)
-$max_slots = "84";
+$weekstarts           = 1;           // Start the week on Monday
+$hidden_days          = array(6,0);  // Hide Saturday (6) and Sunday (0) as the meeting rooms are not available on these days
+$dateformat           = 1;           // Display short dates in non-US format
+$default_view         = "month";     // Set the default view to month
+$month_cell_scrolling = FALSE;       // Have cells expand to display all their bookings rather than present a scroll bar
+$max_slots            = "84";        // Maximum number of booking slots per day, based on the start of first and start of last slots, default duration and booking resolution, 
+                                     // all of which are defined in Rooms > Area > Edit
+                                     // 8am to 9pm = 13 hours, default duration = 1 hour, 10 minute booking resolution = 6 slots per hour
+                                     // 6(13+1)=84
 
 
-/************************
- * Private field settings
- ************************/
-
-$is_private_field['entry.name']        = TRUE;  // Client name
-$is_private_field['entry.description'] = TRUE;  // Additional information
-$is_private_field['entry.create_by']   = FALSE; // Therapist name
-
-
-/**********************************************
- * Email settings
- **********************************************/
-
-// WHO TO EMAIL
-// ------------
-// The following settings determine who should be emailed when a booking is made,
-// edited or deleted (though the latter two events depend on the "When" settings below).
-// Set to TRUE or FALSE as required
-// (Note:  the email addresses for the room and area administrators are set from the
-// edit_area_room.php page in MRBS)
-$mail_settings['admin_on_bookings']      = TRUE;  // the addresses defined by $mail_settings['recipients'] below
-$mail_settings['area_admin_on_bookings'] = FALSE;  // the area administrator
-$mail_settings['room_admin_on_bookings'] = FALSE;  // the room administrator
-$mail_settings['booker']                 = TRUE;  // the person making the booking
-$mail_settings['book_admin_on_approval'] = FALSE;  // the booking administrator when booking approval is enabled
-                                                   // (which is the MRBS admin, but this setting allows MRBS
-                                                   // to be extended to have separate booking approvers)     
-
-// WHEN TO EMAIL
-// -------------
-// These settings determine when an email should be sent.
-// Set to TRUE or FALSE as required
-//
-// (Note:  (a) the variables $mail_settings['admin_on_delete'] and
-// $mail_settings['admin_all'], which were used in MRBS versions 1.4.5 and
-// before are now deprecated.   They are still supported for reasons of backward
-// compatibility, but they may be withdrawn in the future.  (b)  the default 
-// value of $mail_settings['on_new'] is TRUE for compatibility with MRBS 1.4.5
-// and before, where there was no explicit config setting, but mails were always sent
-// for new bookings if there was somebody to send them to)
-
-$mail_settings['on_new']    = FALSE;   // when an entry is created
-$mail_settings['on_change'] = FALSE;  // when an entry is changed
-$mail_settings['on_delete'] = FALSE;  // when an entry is deleted
+/*****************************
+ * Confidential field settings
+ *****************************/
+$is_private_field['entry.name']        = TRUE;   // Client name
+$is_private_field['entry.description'] = TRUE;   // Additional information
+$is_private_field['entry.create_by']   = FALSE;  // Therapist name
 
 
-// WHAT TO EMAIL
-// -------------
-// These settings determine what should be included in the email
-// Set to TRUE or FALSE as required
-$mail_settings['details']   = FALSE; // Set to TRUE if you want full booking details;
-                                     // otherwise you just get a link to the entry
-$mail_settings['html']      = FALSE; // Set to true if you want HTML mail
-$mail_settings['icalendar'] = FALSE; // Set to TRUE to include iCalendar details
-                                     // which can be imported into a calendar.  (Note:
-                                     // iCalendar details will not be sent for areas
-                                     // that use periods as there isn't a mapping between
-                                     // periods and time of day, so the calendar would not
-                                     // be able to import the booking)
+/*****************
+ * Email addresses
+ *****************/
+$mail_settings['from']       = "The Harbour <harbour@robash.me.uk>";  // From address shown in emails sent by MRBS
+$mail_settings['recipients'] = "The Harbour <harbour@robash.me.uk>";  // Admin email address
 
-// Set this to TRUE if you want MRBS to output debug information to the browser
-// when you are sending email.   If you are not getting emails it can be helpful
-// by telling you (a) whether the mail functions are being called in the first place
-//(b) whether there are addresses to send email to and (c) the result of the mail
-// sending operation.
+
+/****************
+ * Email policies
+ ****************/
+$mail_settings['admin_on_bookings']      = TRUE;   // Email administrator on new booking
+$mail_settings['area_admin_on_bookings'] = FALSE;  // Email area administrators on new booking, disabled as we have a single area
+$mail_settings['room_admin_on_bookings'] = FALSE;  // Email room administrators on new booking, disabled as we have a single administrator for all rooms
+$mail_settings['booker']                 = TRUE;   // Email the person making the booking
+$mail_settings['book_admin_on_approval'] = FALSE;  // Email a separate administrator if approval is required, disabled as all bookings require approval
+$mail_settings['on_new']                 = FALSE;  // Email when a booking is created
+$mail_settings['on_change']              = FALSE;  // Email when a booking is changed
+$mail_settings['on_delete']              = FALSE;  // Email when a booking is deleted
+$mail_settings['details']                = FALSE;  // Include a link to the booking in emails, not all the booking details
+$mail_settings['html']                   = FALSE;  // Send emails in plain text
+$mail_settings['icalendar']              = FALSE;  // Do not include an attachment with iCalendar details
+
+
+/****************************
+ * Email development settings
+ ****************************/
 $mail_settings['debug'] = TRUE;
-// Set this to TRUE if you do not want any email sent, whatever the rest of the settings.
-// This is a global setting that will override anything else.   Useful when testing MRBS.
 $mail_settings['disabled'] = TRUE;
-
-// EMAIL - MISCELLANEOUS
-// ---------------------
-
-// Set the email address of the From field. Default is 'admin_email@your.org'
-$mail_settings['from'] = 'The Harbour <harbour@robash.me.uk>';
-
-// Set the recipient email. Default is 'admin_email@your.org'. You can define
-// more than one recipient like this "john@doe.com,scott@tiger.com"
-$mail_settings['recipients'] = 'The Harbour <harbour@robash.me.uk>';
 
 
 /*************
  * Reports
  *************/
-
-// CSV format
-// By default Excel expects a tab as the column separator, so if you are opening
-// CSV files with Excel you may want to change $csv_col_sep to be '\t'
-$csv_col_sep = "\t";   // Separator between columns/fields
-
-// Default report span in days:
-$default_report_days = 30;
+$csv_col_sep         = "\t";  // Include the right separator between columns/fields so exported CSV files are handled correctly in Microsoft Excel
+$default_report_days = 30;    // Default number of days a report should cover
 
 
 /**********************************************
  * Vocabulary
+ * Use this section to overide vocabulary defined in lang/lang.en
  **********************************************/
-
-$vocab_override['en']['namebooker'] = "Client name (kept private)"; // Used to overide vocabulary on edit_entry.php
-$vocab_override['en']['fulldescription'] = "Additional information (kept private)"; // Used to overide vocabulary on edit_entry.php
-$vocab_override['en']['description'] = "Additional information"; // Used to overide vocabulary on view_entry.php
-$vocab_override['en']['unavailable'] = "Confidential"; // Used to overide vocabulary in functions_table.inc
-$vocab_override['en']['private'] = "Confidential"; // Used to overide vocabulary on view_entry.php
-$vocab_override['en']['norights'] = "You do not have the necessary rights to view this page. If you are having problems logging in please contact <a href=mailto:info@the-harbour.co.uk>info@the-harbour.co.uk</a>.";
-
+$vocab_override['en']['namebooker']      = "Client name (kept private)";             // Used to overide vocabulary on edit_entry.php
+$vocab_override['en']['fulldescription'] = "Additional information (kept private)";  // Used to overide vocabulary on edit_entry.php
+$vocab_override['en']['description']     = "Additional information";                 // Used to overide vocabulary on view_entry.php
+$vocab_override['en']['unavailable']     = "Confidential";                           // Used to overide vocabulary in functions_table.inc
+$vocab_override['en']['private']         = "Confidential";                           // Used to overide vocabulary on view_entry.php
+$vocab_override['en']['norights']        = "You do not have the necessary rights to view this page. If you are having problems logging in please contact <a href=mailto:info@the-harbour.co.uk>info@the-harbour.co.uk</a>.";
 
 ?>
